e74310f9980ece0db3e989606b1c67db7da18e9c
dmittryyyy <hhh963123741@gmail.com>
2023-12-11 12:56:10 +0700

    create structure project and add new files
    add styles, change structure, add sprites

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import { App } from './views/App/App.tsx'
+
+import './views/styles/reset.scss';
+import './views/styles/common.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/views/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import styles from './index.module.scss';
+
+export const App: React.FC = () => {
+  return (
+      <main className={styles.main}>
+          <h1 className={styles.mainTitle}>To Do</h1>
+          <section className={styles.mainSection}>
+
+          </section>
+          <section className={styles.mainSection}>
+
+          </section>
+      </main>
+  );
+};
